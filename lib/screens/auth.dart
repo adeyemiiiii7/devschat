796e451892d199ec05db2f1efffc1d5d4c0d1e12
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-14 23:50:26 -0400

    intialize firebase on my main dart file

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:devschat/firebase_options.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
