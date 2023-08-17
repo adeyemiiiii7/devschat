3979458055f3dfedde8a1fef4393b5c3b31f5e24
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-08-16 23:16:08 -0400

    image picker

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -11,40 +11,50 @@ class UserImagePicker extends StatefulWidget {
 
   @override
   State<UserImagePicker> createState() => _UserImagePickerState();
+  // Declare a function onPickImage which takes a File argument
   final void Function(File pickedImage) onPickImage;
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  // Declare a variable to store the picked image file
   File? _pickedImageFile;
 
+  // Create a private function _pickImage to pick an image from the camera
   void _pickImage() async {
+    // Use ImagePicker to pick an image from the camera
     final pickedImage = await ImagePicker().pickImage(
       source: ImageSource.camera,
       imageQuality: 50,
       maxWidth: 150,
     );
 
+    // Check if an image was picked
     if (pickedImage == null) {
       return;
     }
 
+    // Set the picked image file and update the state
     setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
 
+    // Call the onPickImage function provided by the parent widget and pass the picked image file
     widget.onPickImage(_pickedImageFile!);
   }
 
   @override
   Widget build(BuildContext context) {
+    // Return a Column widget with children
     return Column(
       children: [
+        // Display a CircleAvatar widget for showing the user's image
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
           foregroundImage:
               _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
+        // Display a TextButton with an icon and label to trigger image picking
         TextButton.icon(
           onPressed: _pickImage,
           icon: const Icon(Icons.image),
